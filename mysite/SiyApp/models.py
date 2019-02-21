ffe2b04c5342445d87cd6159847b13f7860fb51d
Poison159 <Siyabongahdb@gmail.com>
2019-02-21 20:07:07 +0200

    Added_forenKeys to models

--- mysite/SiyApp/models.py
+++ mysite/SiyApp/models.py
@@ -9,20 +9,23 @@ class Tournament(models.Model):
         return self.name    
 
 class Team(models.Model):
+    tournament      = models.ForeignKey(Tournament, on_delete=models.CASCADE,null=True,default=2040)
     name            = models.CharField(max_length=128,unique=True)
     kasi            = models.CharField(max_length=64)
-    
+    imgPath         = models.CharField(max_length=128,default="./Content/imgs/fcb.jpg")
     def __str__(self):
         return self.name
     
 class Player(models.Model):
+    Team     = models.ForeignKey(Team, on_delete=models.CASCADE,null=True,default=2040)
     name     = models.CharField(max_length=128,unique=True)
+    imgPath  = models.CharField(max_length=128,default="./Content/imgs/fcb.jpg")
     age      = models.IntegerField()
-    
     def __str__(self):
         return self.name
 
 class Fixture(models.Model):
+    tournament      = models.ForeignKey(Tournament, on_delete=models.CASCADE,null=True,default=2040)
     homeTeam        = models.CharField(max_length=128)
     awayTeam        = models.CharField(max_length=128)
     date            = models.DateField()
@@ -32,6 +35,7 @@ class Fixture(models.Model):
         return self.pitch + self.stage
 
 class Result(models.Model):
+    fixture         = models.ForeignKey(Fixture, on_delete=models.CASCADE,null=True,default="1255")
     homeGoals       = models.IntegerField()
     awayGaols       = models.IntegerField()
     def __str__(self):
