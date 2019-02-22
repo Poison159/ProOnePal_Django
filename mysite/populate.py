77cb289f3ff30304271983981562c55184096cd4
Poison159 <Siyabongahdb@gmail.com>
2019-02-22 19:50:48 +0200

    Changed_populate_script

--- mysite/populate.py
+++ mysite/populate.py
@@ -12,20 +12,21 @@ def getDataFromCsv(path,entity):
         List = []
         if (entity == 'Teams'):
                 for line in fd:
-                        elems = line.split(',')
-                        elems = elems.pop(0)
-                        team = Team.objects.get_or_create(name = elems[1],
-                        kasi = elems[2])
-                        List.append(Team(name = elems[1],
-                        kasi = elems[2]))
+                        if(i != 0):
+                                elems = line.split(',')
+                                nameTrimed = elems[1].split('"')[1]
+                                kasiTrimed = elems[2].split('"')[1]
+                                team = Team.objects.get_or_create(name = nameTrimed,kasi = kasiTrimed)
+                                List.append(Team(name = nameTrimed,kasi = kasiTrimed))
+                        i = i + 1
         if (entity == 'Players'):
                 for line in fd:
-                        elems = line.split(',')
-                        elems = elems.pop(0)
-                        player = Player.objects.get_or_create(name = elems[1],
-                        age = elems[2], position = elems[3])
-                        List.append(Player(name = elems[1],
-                        kasi = elems[2],position = elems[3]))
+                        if(i != 0):
+                                elems   = line.split(',')
+                                ageInt  = int(elems[2].split('"')[1])
+                                player  = Player.objects.get_or_create(name = elems[1],age = ageInt, position = elems[3])
+                                List.append(Player(name = elems[1],age = ageInt,position = elems[3]))
+                        i = i + 1
         if (entity == 'Tournaments'):
                 for line in fd:
                         if(i != 0):
@@ -37,23 +38,24 @@ def getDataFromCsv(path,entity):
                                 tournament      = Tournament.objects.get_or_create(name = nameTrimed,maxStages = maxStagesInt,maxGames = maxGamesInt,maxTeams = maxTeamsInt)
                                 List.append(Tournament(name = nameTrimed,maxStages = maxStagesInt,maxGames = maxGamesInt,maxTeams = maxTeamsInt))
                         i = i + 1
-                        
-        
         if (entity == 'Fixtures'):
                 for line in fd:
-                        elems = line.split(',')
-                        elems = elems.pop(0)
-                        fixture = Fixture.objects.get_or_create(stage = elems[1],homeTeam = elems[2],
-                        awayTeam = elems[3],date = elems[4],pitch = elems[5])
-                        List.append(Fixture(stage = elems[1],homeTeam = elems[2],
-                        awayTeam = elems[3],date = elems[4],pitch = elems[5]))
-
+                        if(i != 0):
+                                elems = line.split(',')
+                                fixture = Fixture.objects.get_or_create(stage = elems[1],homeTeam = elems[2],
+                                awayTeam = elems[3],date = elems[4],pitch = elems[5])
+                                List.append(Fixture(stage = elems[1],homeTeam = elems[2],
+                                awayTeam = elems[3],date = elems[4],pitch = elems[5]))
+                        i = i + 1
         if (entity == 'Results'):
                 for line in fd:
-                        elems = line.split(',')
-                        elems = elems.pop(0)
-                        result = Result.objects.get_or_create(homeGoals = int(elems[1].strip('"')),awayGoals = int(elems[2]))
-                        List.append(Result(homeGoals = int(elems[1]),awayGoals = int(elems[2])))  
+                        if(i != 0):
+                                elems = line.split(',')
+                                homeGoalsInt = int(elems[1].split('"')[1])
+                                awayGoalsInt = int(elems[2].split('"')[1])
+                                result = Result.objects.get_or_create(homeGoals = homeGoalsInt,awayGoals = awayGoalsInt)
+                                List.append(Result(homeGoals = homeGoalsInt,awayGoals = awayGoalsInt))
+                        i = i + 1
 
         return List                
 
@@ -62,5 +64,5 @@ if __name__ == '__main__':
     print("Population Script Running")
     enties = {"Teams","Players","Tournaments","Results","Fixtures"}
     for entity in enties:
-                getDataFromCsv("C:\\Users\\Siya\\Documents\\ProOnePalCSV","Tournaments")
+                getDataFromCsv("C:\\Users\\Siya\\Documents\\ProOnePalCSV","Teams")
     print("Population Complete!!")
